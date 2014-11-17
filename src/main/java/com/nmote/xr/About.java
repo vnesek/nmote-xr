c231b51d8d0cd1bab434e66ed53183641e31d512
Vjekoslav Nesek <vnesek@instantcom.net>
2014-11-17 17:36:56 +0100

    Upped version to 2.1.0 Beta and updated dependencies.

--- src/main/java/com/nmote/xr/About.java
+++ src/main/java/com/nmote/xr/About.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Nmote Ltd. 2003-2014. All rights reserved. 
+ * Copyright (c) Nmote Ltd. 2003-2014. All rights reserved.
  * See LICENSE doc in a root of project folder for additional information.
  */
 
@@ -9,16 +9,16 @@ import java.io.Serializable;
 
 public final class About implements Serializable {
 
-	public static final long serialVersionUID = 200L;
-	
+	public static final long serialVersionUID = 210L;
+
 	public static final String COPYRIGHT = "Copyright (c) 2005-2014, Nmote ltd., All rights reserved";
 	public static final String NAME = "nmote-xr";
-	public static final String VERSION = "2.0.0";
+	public static final String VERSION = "2.1.0.Beta";
 
 	private About() {}
-	
+
 	public static void main(String[] args) {
 		System.out.println(NAME + "/" + VERSION);
 		System.out.println(COPYRIGHT);
-	} 
+	}
 }
